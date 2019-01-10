e30c58db78257113c9bacb876246265bee48513f
Vorihery <rico.tonda@gmail.com>
2019-01-10 18:07:04 +0300

    modif auth routes

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -111,7 +111,7 @@
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">S'enregistrer</a></li>
+                    <!-- <li><a href="{{ url('/register') }}">S'enregistrer</a></li> -->
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
